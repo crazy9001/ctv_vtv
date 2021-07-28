86ed7583fa00ae586e03141f8e0a0f2a8c8fea6d
Toinn <ngoctoi.it@gmail.com>
2021-07-28 16:31:46 +0700

    add breadcrum

--- platform/themes/vtv_college/partials/short-codes/gallery-media.blade.php
+++ platform/themes/vtv_college/partials/short-codes/gallery-media.blade.php
@@ -1,37 +1,9 @@
 @php $galleries = get_galleries(5); @endphp
-{{--@if (!$galleries->isEmpty())--}}
-{{--    <section class="section-home">--}}
-{{--        <div class="bg-media">--}}
-{{--            <div class="title d-flex justify-content-center">--}}
-{{--                <h3>{{ $title }}</h3>--}}
-{{--            </div>--}}
-{{--            <div class="media-content d-flex justify-content-center">--}}
-{{--                <div class="text-content text-center">--}}
-{{--                      <span>--}}
-{{--                        {{ $description }}--}}
-{{--                      </span>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="w-100 d-flex justify-content-center">--}}
-{{--                <div id="wrapper_bu">--}}
-{{--                    @foreach($galleries as $index => $gallery)--}}
-{{--                        <div id="bu{{$index +1}}">--}}
-{{--                            <div class="w-100 h-100" style="position: relative">--}}
-{{--                                <img class="img-resize" src="{{ RvMedia::getImageUrl($gallery->image, 'featured') }}" alt="{{ $gallery->name }}">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    @endforeach--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </section>--}}
-{{--@endif--}}
-
 @if (!$galleries->isEmpty())
     <section class="section-home">
     <div class="bg-media">
         <div class="title d-flex justify-content-center">
-            <h3>>{{ $title }}</h3>
+            <h3>{{ $title }}</h3>
         </div>
         <div class="media-content d-flex justify-content-center">
             <div class="text-content text-center">
