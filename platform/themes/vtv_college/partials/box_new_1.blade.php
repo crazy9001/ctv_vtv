d53d56b16a410faf74e009be505a8e9559e0b4aa
Toinn <ngoctoi.it@gmail.com>
2021-08-05 18:57:00 +0700

    fix responsive

--- platform/themes/vtv_college/partials/box_new_1.blade.php
+++ platform/themes/vtv_college/partials/box_new_1.blade.php
@@ -38,28 +38,29 @@
                     </div>
                 </div>
 
-
-                <div class="row">
-                    @else
-                        <div class="col-xl-3 p-0 col-lg-4 col-md-6 flex-column item-new-ps col-sm-6 col-6">
-                            <div class="bg-img">
-                                <div class="d-flex align-items-center justify-content-center">
-                                    <div class="card-image-event w-100 h-100">
-                                        <a href="{{ $featureItem->url }}">
-                                            <img src="{{ RvMedia::getImageUrl($featureItem->image, 'featured', false, RvMedia::getDefaultImage()) }}" class="img-resize img-shadow-top"/>
-                                        </a>
+                <div class="item-content-event">
+                    <div class="row ml-0 mr-0">
+                        @else
+                            <div class="col-xl-3 p-0 col-lg-4 col-md-6 flex-column item-new-ps col-sm-6 col-6">
+                                <div class="bg-img">
+                                    <div class="d-flex align-items-center justify-content-center">
+                                        <div class="card-image-event w-100 h-100">
+                                            <a href="{{ $featureItem->url }}">
+                                                <img src="{{ RvMedia::getImageUrl($featureItem->image, 'featured', false, RvMedia::getDefaultImage()) }}" class="img-resize img-shadow-top"/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="time-item-ps d-flex flex-row align-items-center">
+                                        <i class="far fa-calendar-alt" style="color: #666666; width: 17px; height: 16px"></i>
+                                        <span class="ml-2">{{ $featureItem->created_at->format('d/m/Y - H:i:s') }}</span>
+                                    </div>
+                                    <div class="content-new-item">
+                                        <h3> <a href="{{ $featureItem->url }}">{{ $featureItem->name }}</a></h3>
                                     </div>
                                 </div>
-                                <div class="time-item-ps d-flex flex-row align-items-center">
-                                    <i class="far fa-calendar-alt" style="color: #666666; width: 17px; height: 16px"></i>
-                                    <span class="ml-2">{{ $featureItem->created_at->format('d/m/Y - H:i:s') }}</span>
-                                </div>
-                                <div class="content-new-item">
-                                    <h3> <a href="{{ $featureItem->url }}">{{ $featureItem->name }}</a></h3>
-                                </div>
                             </div>
-                        </div>
-                    @if ($loop->last)
+                        @if ($loop->last)
+                    </div>
                 </div>
             @endif
             @endif
--- platform/themes/vtv_college/partials/box_new_3.blade.php
+++ platform/themes/vtv_college/partials/box_new_3.blade.php
@@ -37,16 +37,16 @@
             <div class="col-lg-6 mt-lg-0 mt-5">
                 @else
                 <div class="tuyensinh-item row mb-4 mr-0 ml-0">
-                    <div class="col-sm-6 p-0 img-tuyensinh-item d-flex justify-content-center">
+                    <div class="col-sm-6 col-6 p-0 img-tuyensinh-item d-flex justify-content-center">
                         <div class="w-100 h-100 card-image">
                             <a href="{{ $featureItem->url }}">
                                 <img src="{{ RvMedia::getImageUrl($featureItem->image, 'featured', false, RvMedia::getDefaultImage()) }}" class="img-resize img-border-radius-left"/>
                             </a>
                         </div>
                     </div>
-                    <div class="col-sm-6 mt-4 d-flex flex-column">
+                    <div class="col-sm-6 col-6 mt-4 d-flex flex-column">
                         <div class="time-item-ps m-0 d-flex flex-row align-items-center">
-                            <i class="far fa-calendar-alt"style="color: #666666; width: 17px; height: 16px"></i>
+                            <i class="far fa-calendar-alt" style="color: #666666; width: 17px; height: 16px"></i>
                             <span class="ml-2">{{ $featureItem->created_at->format('d/m/Y - H:i:s') }}</span>
                         </div>
                         <div class="content-item-tuyensinh">
